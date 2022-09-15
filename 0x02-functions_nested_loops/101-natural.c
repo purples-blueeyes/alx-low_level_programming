ea921d2a74f66ced08db38df7dda9e5313020644
Anthoine <dzimahanthoine@gmail.com>
2022-09-14 17:35:32 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - check for miltiples of 3 and 5
+ * Return: 0 (Always)
+ */
+
+int main(void)
+
+{
+
+	int a = 1024, b, sum = 0;
+
+	for (b = 0; b < a; b++)
+	
+	{
+
+		if ((b % 3 == 0) || (b % 5 == 0))
+
+		{
+
+			sum = sum + b;
+
+		}
+
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+
+}
