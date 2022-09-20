434ac0b66fa1502e82375bf7077dee50e284b9b0
Anthoine <dzimahanthoine@gmail.com>
2022-09-19 18:41:28 -0700

    function that takes a pointer to an int

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to an int
+ * @n: parameter
+ * updates the value it points to 98
+ * 
+ * Return: 0 (Always)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
