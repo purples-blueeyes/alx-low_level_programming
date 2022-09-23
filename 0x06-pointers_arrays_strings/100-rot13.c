4e4d9a5c67cabce25a0b0b44b11596dd36e05098
Anthoine <dzimahanthoine@gmail.com>
2022-09-22 20:36:10 -0700

    8 task

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -17,11 +17,11 @@ char *rot13(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; a[j] != '\0'; j++)
+		for (j = 0; b[j] != '\0'; j++)
 		{
-			if (s[i] == a[j])
+			if (s[i] == b[j])
 			{
-				s[i] = b[j];
+				s[i] = c[j];
 				break;
 			}
 		}
