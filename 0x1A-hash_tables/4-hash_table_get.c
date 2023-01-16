b7f64b195dc194dd935641f33b4852d5ecfa4a68
Anthoine <dzimahanthoine@gmail.com>
2023-01-16 06:01:49 -0800

    function that retrieves a value associated with a key.

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,12 +1,12 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - Add or update an element in a hash table.
+ * hash_table_set - Retrieve the value associated with
+ * a key in a hash table.
  * @ht: A pointer to the hash table.
- * @key: The key to add - cannot be an empty string.
- * @value: The value associated with key.
- * Return: Upon failure - 0.
- * Otherwise - 1.
+ * @key: The key to get the value of.
+ * Return: If the key cannot be matched - NULL.
+ * Otherwise - the value associated with key in ht.
  */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
